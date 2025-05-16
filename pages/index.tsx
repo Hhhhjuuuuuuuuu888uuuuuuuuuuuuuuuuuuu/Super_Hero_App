fd8a2462495ee1bef5ec38589bfd8ff9b85b91ae
Collins Matlala <121460963+Hhhhjuuuuuuuuu888uuuuuuuuuuuuuuuuuuu@users.noreply.github.com>
2025-05-16 17:38:01 +0200

    changes

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import '../styles/myStyle.css'
+
+export default function Home() {
+  return (
+    <div className="header">
+      <h1>Hello World</h1>
+      <p>This is styled with my own CSS.</p>
+    </div>
+  )
+}
